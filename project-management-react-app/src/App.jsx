1e94f0813f1e5a8084bba79c68d80fc4203b29ba
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-07-16 00:18:26 +0100

    Worked on adding & deleting tasks

--- project-management-react-app/src/App.jsx
+++ project-management-react-app/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
     })
   }
 
-  function handleProjectDeletion(id){
+  function handleProjectDeletion(){
     setProjectState(prevProjectState => {
       return {
         ...prevProjectState,
