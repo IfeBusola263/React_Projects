d9725fccd2d947f4efee6ea830a84a8bc0fcb60d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-07-15 21:54:22 +0100

    Worked on Modal Error and New Project Component

--- project-management-react-app/src/App.jsx
+++ project-management-react-app/src/App.jsx
@@ -1,8 +1,62 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSideBar from "./components/ProjectsSideBar";
+
 function App() {
+
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject(){
+    setProjectState(prevProjectState => {
+      return {
+        ...prevProjectState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  function handleAddProject(projectDetails){
+    setProjectState(prevProjectState => {
+
+      const newProject = {
+        ...projectDetails,
+        id: Math.random()
+      }
+
+      return {
+        ...prevProjectState,
+        selectedProjectId: undefined,
+        projects: [...prevProjectState.projects, newProject]
+      }
+    })
+  }
+
+  function handleCancelAction(){
+    setProjectState(prevProjectState => {
+      return {
+        ...prevProjectState,
+        selectedProjectId: undefined
+      }
+    })
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null){
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAction} />;
+  } else if (projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSideBar projects={projectState.projects} onStartAddProject={handleStartAddProject} />
+      {content}
+    </main>
   );
 }
 
---
+++ project-management-react-app/src/components/NewProject.jsx
@@ -0,0 +1,46 @@
+import Input from "./Input";
+import { useRef } from "react";
+import Modal from "./Modal";
+
+export default function NewProject({onAdd, onCancel}) {
+
+    const title = useRef();
+    const desc = useRef();
+    const dueDate = useRef();
+    const errorModal = useRef();
+
+    function handleSave(){
+        const enteredTitle = title.current.value;
+        const enteredDesc = desc.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        if (enteredTitle.trim() === '' || enteredDesc.trim() === '' || enteredDueDate.trim() === ''){
+            errorModal.current.open();
+            return;
+        }
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDesc,
+            dueDate: enteredDueDate
+        })
+    }
+
+  return (
+    <>
+    <Modal ref={errorModal} buttonCaption="Close"> 
+        <h2 className="text-xl font-bold text-stone-500 my-4">Invalid Input</h2>
+        <p className="text-stone-400 mb-4">Please fill all the fields</p>
+    </Modal>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li><button onClick={onCancel} className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+        <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+      </menu>
+      <Input ref={title} type='text' label="Title" />
+      <Input ref={desc} label="Description" textarea />
+      <Input ref={dueDate} type='date' label="Due Date" />
+    </div>
+    </>
+  );
+}
