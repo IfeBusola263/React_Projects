5dae4c5b630696d92e78e2a5db62f60e51ecfefc
Busola <ifebusolaj95@gmail.com>
2024-08-05 23:26:37 +0100

    Backend for events app

--- react-router-app/backend/app.js
+++ react-router-app/backend/app.js
@@ -1,17 +1,21 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 
 const eventRoutes = require('./routes/events');
+const authRoutes = require('./routes/auth');
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   next();
 });
+
+app.use(authRoutes);
 
 app.use('/events', eventRoutes);
 
